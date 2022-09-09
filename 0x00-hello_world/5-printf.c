b1fc408e31258c6817dac2e1dde4c30c986ca83c
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-09-08 17:37:11 -0700

    5-print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Betty
+ * Return:0 when function is success
+ */
+int main(void);
+{
+printf("with proper grammar,but the outcome is a piece of art,\n");
+return (0);
