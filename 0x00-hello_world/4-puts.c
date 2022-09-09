e2089be19754ef9c2ed4750289154abd991aae65
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-09-08 17:20:40 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main-Entry point
+ * Betty
+ * Return:0 when completed
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like buildinga multilingual puzzle");
 return (0);
 }
