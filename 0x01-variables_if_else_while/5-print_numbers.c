2e19be714bc3fc5bf2dc7698050488708a35e7aa
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-09-09 16:58:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * display all single digits
+ * Return: 0 success
+ */
+int main(void)
+{
+int n = 48;
+while (n < 58)
+{
+putchar (n);
+n++;
+}
+putchar (10);
+return (0);
+}
