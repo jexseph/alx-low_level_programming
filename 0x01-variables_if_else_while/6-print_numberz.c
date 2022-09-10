46b8fbbc109421299b797467524e61670cf35a6f
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-09-09 17:18:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * single digits are to be printed
+ * Return: 0 when sucessful
+ */
+int main(void)
+{
+int x = 48;
+while (x < 58)
+{
+putchar (x);
+x++;
+}
+putchar (10);
+return (0);
+}
