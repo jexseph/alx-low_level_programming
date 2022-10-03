3edc77ef17427804f310bcc6d7dc477c5e733f73
Ifeoma Adigwe <ifeomaadigwe91@gmail.com>
2022-10-03 09:29:26 -0700

    main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int size,char c);
+char *strdup(char *str);
+int **alloc_grid(int width, int height);
+char *str_concat(chr *s1, char *s2);
+void free_grid(int **grid, int height);
+
+#endif
